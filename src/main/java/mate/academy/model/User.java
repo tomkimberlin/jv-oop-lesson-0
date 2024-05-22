a09efa5875434cfe7fea73a45e057a76c50e1872
tomkimberlin <6531977+tomkimberlin@users.noreply.github.com>
2024-05-22 15:13:37 -0500

    formatting

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -1,19 +1,19 @@
 package mate.academy.model;
 
 public class User {
-    private String email;
-    private String password;
+  private String email;
+  private String password;
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
+  public User(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 }
